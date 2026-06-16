eec1bc595be7c467292ee1572c38b2e9e327e3bb
Niranjan Yadav <niranjan.yadav.dev@gmail.com>
2026-06-16 12:57:14 +0530

    Enhance delivery tracking with parcel management

--- deliveryTracking.java
+++ deliveryTracking.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DeliveryTracking {
@@ -6,48 +7,78 @@ public class DeliveryTracking {
 
         Scanner sc = new Scanner(System.in);
 
-        String parcelId = "";
-        String status = "No Parcel";
+        ArrayList<String> parcelIds = new ArrayList<>();
+        ArrayList<String> statusList = new ArrayList<>();
 
         int choice;
 
         do {
-
             System.out.println("\n1. Add Parcel");
             System.out.println("2. Update Status");
             System.out.println("3. Track Parcel");
-            System.out.println("4. Exit");
+            System.out.println("4. Show All Parcels");
+            System.out.println("5. Exit");
 
             System.out.print("Enter Choice: ");
             choice = sc.nextInt();
 
-            switch(choice) {
+            switch (choice) {
 
                 case 1:
                     System.out.print("Enter Parcel ID: ");
-                    parcelId = sc.next();
-                    status = "Dispatched";
-                    System.out.println("Parcel Added");
+                    String id = sc.next();
+
+                    parcelIds.add(id);
+                    statusList.add("Dispatched");
+
+                    System.out.println("Parcel Added Successfully!");
                     break;
 
                 case 2:
-                    status = "Delivered";
-                    System.out.println("Status Updated");
+                    System.out.print("Enter Parcel ID: ");
+                    id = sc.next();
+
+                    int index = parcelIds.indexOf(id);
+
+                    if (index != -1) {
+                        statusList.set(index, "Delivered");
+                        System.out.println("Status Updated!");
+                    } else {
+                        System.out.println("Parcel Not Found!");
+                    }
                     break;
 
                 case 3:
-                    System.out.println("Parcel ID: " + parcelId);
-                    System.out.println("Status: " + status);
+                    System.out.print("Enter Parcel ID: ");
+                    id = sc.next();
+
+                    index = parcelIds.indexOf(id);
+
+                    if (index != -1) {
+                        System.out.println("Parcel ID: " + parcelIds.get(index));
+                        System.out.println("Status: " + statusList.get(index));
+                    } else {
+                        System.out.println("Parcel Not Found!");
+                    }
                     break;
 
                 case 4:
-                    System.out.println("Exit");
+                    System.out.println("\nAll Parcels:");
+                    for (int i = 0; i < parcelIds.size(); i++) {
+                        System.out.println(parcelIds.get(i) + " - " + statusList.get(i));
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
                     break;
 
                 default:
                     System.out.println("Invalid Choice");
             }
 
-        } while(choice != 4);
+        } while (choice != 5);
+
+        sc.close();
     }
 }
